557056ebdd8638b74beb16010248f5acd6da59ad
Tobi Medeiros <t.medow710@gmail.com>
2025-02-07 16:05:36 -0500

    fix procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: python main.py
+web: waitress-serve --host=0.0.0.0 --port=5000 main:app
+
--- main.py
+++ main.py
@@ -101,4 +101,4 @@ def update_map():
 if __name__ == "__main__":
     #app.run(host="0.0.0.0", port=5000, debug=True)
     #app.run(debug=True)
-    serve(app, host="0.0.0.0", port=5000)
+    pass
