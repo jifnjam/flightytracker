66b4f6a70122803756eed5cc5920fd724dc30402
Tobi Medeiros <t.medow710@gmail.com>
2025-02-08 03:03:04 -0500

    found error!

--- main.py
+++ main.py
@@ -113,5 +113,5 @@ if __name__ == "__main__":
     #app.run(host="0.0.0.0", port=5000, debug=True)
     #app.run()
     #pass
-    #serve(app, host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
-    serve(app, host='0.0.0.0', port=5000)
+    serve(app, host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    #serve(app, host='0.0.0.0', port=5000)
