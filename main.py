0c16dea9a89d9606e2264fa450139a8752779221
Tobi Medeiros <t.medow710@gmail.com>
2025-02-10 16:35:58 -0500

    edit index.html

--- main.py
+++ main.py
@@ -19,90 +19,9 @@ import os
 
 app = Flask(__name__) #constructor for flask webapp
 
-url = "https://otmmint:CFuMr9M84mXigLz@opensky-network.org/api/states/all?lamin=30.038&lomin=-125.974&lamax=52.214&lomax=-68.748"
-
-#DATA FRAME CONVERSION
-def wgs84_to_web_mercator(df, lon="longitude", lat="latitude"):
-    k = 6378137
-    df["x"] = df[lon] * (k * np.pi/180.0)
-    df["y"] = np.log(np.tan((90 + df[lat]) * np.pi/360.0)) * k
-    return df
-
-
-def get_flights():
-     """Use opensky network api to get flight data"""
-     r = requests.get(url)
-     data = r.json()
-     df = pd.DataFrame(data['states']) #states is data I need # number after corresponds to state vector
-     fdf = pd.DataFrame( {"icao24": df[0],
-                         "callsign": df[1],
-                         "ctry": df[2],
-                         "longitude": df[5],
-                         "latitude": df[6],
-                         "velocity": df[9]
-                         })
-     location = pd.DataFrame({"longitude": df[5],
-                         "latitude": df[6]})
-     
-     
-     mercator = wgs84_to_web_mercator(fdf)
-     #print(mercator.head())
-     return mercator
-
-def flight_map(doc):
-     """Make a map"""
-
-     loc_df = get_flights()
-
-     flight_source = ColumnDataSource(data=loc_df)
-     p = figure(x_range=(-15187814, -6458032), y_range=(2505715, 6567666),
-               x_axis_type="mercator", y_axis_type="mercator", sizing_mode="scale_both", height=500)
-
-     p.add_tile("CartoDB Positron", retina=True)
-
-     p.scatter(x='x', y='y', size=8, color="indigo", source=flight_source)
-
-     my_hover=HoverTool()
-     my_hover.tooltips=[('Call Sign','@callsign'),('Origin Country','@ctry'),('Velocity(m/s)','@velocity')]
-     p.add_tools(my_hover)
-     
-     def update():
-          new_data = get_flights()
-          flight_source.data = new_data.to_dict(orient="list")
-
-     doc.add_root(p)       
-     doc.add_periodic_callback(update, 5000)
-
-     logging.info("Bokeh flight map added to doc.")
-
-
-     #return p, flight_source 
-     #return file_html(p, CDN, "Flight Map")
-
-bokeh_app = Application(FunctionHandler(flight_map))
-
-# Run Bokeh server in background
-def bk_worker():
-    ioloop = IOLoop.current()
-    port = int(os.getenv("BOKEH_PORT", 5006))
-    server = Server({'/bkapp': bokeh_app}, io_loop=ioloop, allow_websocket_origin=["*"], port=port) 
-    server.start()
-    ioloop.start()
-
-# Ensure the Bokeh server starts before Flask
-thread = threading.Thread(target=bk_worker, daemon=True)
-thread.start()
-
 @app.route("/")
 def index():
      return render_template("index.html") # flight map file
 
-@app.route("/bkapp")
-def update_map():
-    """Return only the updated Bokeh map to be fetched."""
-    #script = server_document('http://localhost:5006/bkapp')  # Embed the Bokeh app
-    script = server_document("/bkapp")
-    return render_template("bokeh-map.html", script=script)
-
 if __name__ == "__main__":
-    app.run(debug=True, port=os.get("PORT", default=5000))
+    app.run(debug=True)
